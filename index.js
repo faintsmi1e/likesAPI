feb2e3f1a0db80881c5c9ff96b0da27a441eb9f8
pinosol <zonest_64@mail.ru>
2021-10-18 19:48:25 +0300

    fixed spaces

--- index.js
+++ index.js
@@ -4,44 +4,40 @@ const port = process.env.PORT || 8080;
 
 const server = new http.Server();
 
-server.on('request', function(req, res) {
-  res.setHeader('Content-type','application/json');
- 
-  
+server.on('request', function (req, res) {
+  res.setHeader('Content-type', 'application/json');
+
   fs.readFile('likes.json', 'utf8', function (error, data) {
     if (!data) {
       const newJson = {
         likes: 0,
       };
       let json = JSON.stringify(newJson);
-      
+
       fs.writeFile('likes.json', json, 'utf8', (err) => {
         console.log('Окей, ты меня запустил. Ты доволен?');
         if (err) {
           console.log(err);
         }
       });
-      
-    }else {
+    } else {
       const likesObject = JSON.parse(data);
-      switch(req.url) {
-          case '/stats': 
-            res.writeHead(200);
-            res.write(JSON.stringify(likesObject));
-            break;
-          case '/like': 
-            res.writeHead(200);
-            likesObject.likes += 1;
-            break;
-          case '/dislike': 
-            res.writeHead(200);
-            likesObject.likes -= 1;
-            break;
-          default : 
-            res.writeHead(404);
-            res.write(JSON.stringify({status : 'err'}));
-
-
+      switch (req.url) {
+        case '/stats':
+          res.writeHead(200);
+          res.write(JSON.stringify(likesObject));
+          break;
+        case '/like':
+          res.writeHead(200);
+          likesObject.likes += 1;
+          break;
+        case '/dislike':
+          res.writeHead(200);
+          likesObject.likes -= 1;
+          break;
+        default:
+          res.writeHead(404);
+          res.write(JSON.stringify({ status: 'err' }));
       }
       let json = JSON.stringify(likesObject);
       fs.writeFile('likes.json', json, 'utf8', (err) => {
@@ -49,13 +45,9 @@ server.on('request', function(req, res) {
           console.log(err);
         }
       });
-      
-      
-    };
+    }
     res.end();
   });
-  
-  
 });
 
 server.listen(port);
